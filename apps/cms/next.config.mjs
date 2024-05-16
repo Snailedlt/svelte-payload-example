450f07a880c3c0c2144ff332c6d1863fc3e297df
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2024-05-16 16:03:29 +0200

    redirect from / to /admin

--- apps/cms/next.config.mjs
+++ apps/cms/next.config.mjs
@@ -3,6 +3,15 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/admin',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default withPayload(nextConfig)
